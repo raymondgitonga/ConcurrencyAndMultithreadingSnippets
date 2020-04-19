f7a8c0abe9a6d73050e60bd786c4acafca070ff0
raymond gitonga <raytosh95@gmail.com>
2020-04-19 04:01:53 +0300

    Add thread interruption and join

--- src/main/java/AnotherThread.java
+++ src/main/java/AnotherThread.java
@@ -2,5 +2,14 @@ public class AnotherThread extends Thread{
     @Override
     public void run() {
         System.out.println(ThreadColor.ANSI_BLUE + "Hello from " + currentThread().getName());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println(ThreadColor.ANSI_BLUE + "Some other thread woke me up");
+            return;
+        }
+
+        System.out.println(ThreadColor.ANSI_BLUE + "Three seconds passed and am now awake");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Thread anotherThread = new AnotherThread();
         anotherThread.setName(" == Another Thread == ");
         anotherThread.start();
+//        anotherThread.interrupt();
 
         Thread newThread = new Thread(()-> System.out.println(ThreadColor.ANSI_GREEN + "Hello from the anonymous thread"));
         newThread.start();
@@ -14,7 +15,15 @@ public class Main {
         Thread runnable = new Thread(new Runnable());
         runnable.start();
 
-        Thread anotherRunnable = new Thread(() -> System.out.println(ThreadColor.ANSI_CYAN + "Hello from an anonymous runnable" ));
+        Thread anotherRunnable = new Thread(() -> {
+            System.out.println(ThreadColor.ANSI_CYAN + "Hello from an anonymous runnable" );
+            try {
+                anotherThread.join();
+                System.out.println(ThreadColor.ANSI_RED + "Another thread terminated so i'm running again");
+            } catch (InterruptedException e) {
+                System.out.println(ThreadColor.ANSI_RED + "I couldn't wait i was interrupted");
+            }
+        });
         anotherRunnable.start();
 
 
